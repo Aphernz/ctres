64399063118d0d7c4ba9189ca6ad3e87844716fc
aphernz <aphernz@users.noreply.github.com>
2018-07-19 21:11:52 -0500

    Initial commit

---
+++ app/src/main/java/com/stadir/ctres/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.stadir.ctres;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate (Bundle saveInstanceState){
+        super.onCreate (saveInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent =new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+
+
+            }
+        },3000);
+
+    }
+}
